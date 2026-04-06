0e232c133d57d37de8ec68be5af9af6723302fa2
Susel Retana <44461708+SuselRetana2726@users.noreply.github.com>
2026-04-06 12:28:12 -0600

    add juego cartas

---
+++ src/components/PopUpLose.vue
@@ -0,0 +1,183 @@
+<template>
+  <div v-if="visible" class="popup-overlay">
+    <div class="popup-box">
+      <div class="popup-fondo">
+        <img
+          src="/images/perdiste.png"
+          alt="icono popup"
+          class="popup-icono"
+        />
+
+        <h2 class="titulo-popup">
+          ¡UPS, CASI LO LOGRAS!
+        </h2>
+
+        <div class="contenido-popup">
+          <p>Te faltó muy poquito para disfrutar de la pizza...</p>
+        </div>
+
+        <div class="contenido-popup" style="margin-top: 0vh !important;">
+          <p class="resultado">
+            <banner class="naranja">Has encontrado </banner><strong>{{ parejas }}</strong><banner class="naranja"> parejas</banner>
+          </p>
+        </div>
+
+        <div class="botones-popup">
+          <button class="boton-iniciar" @click="iniciar">Reiniciar</button>
+          <button class="boton-salir" @click="cerrar">Salir</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: Boolean,
+    parejas: {
+      type: Number,
+      default: 0
+    }
+  },
+  emits: ['cerrar', 'iniciar'],
+  methods: {
+    cerrar() {
+      this.$emit('cerrar');
+    },
+    iniciar() {
+      this.$emit('iniciar');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.popup-overlay {  
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(207, 60, 5, 0.7);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 999;
+}
+
+.popup-box {    
+  position: relative;
+  background: #ffffff;
+  border-radius: 2vh;
+  padding: 3vh 2.5vh;
+  width: 55%;
+  max-width: 60vw; 
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.25);
+  text-align: center;
+}
+
+.popup-fondo {
+  background-image: url('/images/fondomodal.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.popup-box h2 {
+  font-size: 3vh;
+  color: #EB6608;
+  font-weight: 900; 
+  line-height: 1.2;
+  margin-bottom: 2vh;
+  text-transform: uppercase;
+}
+
+.titulo-popup {
+  font-size: 2.8vh; /* más pequeño */
+  color: #EB6608;
+  margin-bottom: 2vh;
+  text-transform: uppercase;
+  font-family: 'EmigreTitulo', sans-serif !important;
+}
+
+.contenido-popup {
+  font-size: 1.8vh;
+  color: #1A1413;
+  text-align: center; /* ← centramos los párrafos */
+  margin-bottom: 1vh;
+  margin-top: 4vh;  
+  font-family: 'EmigreTxt', sans-serif !important
+}
+
+.contenido-popup p{
+  margin: 0.4vh 0;
+}
+.contenido-popup ol {
+  padding-left: 1.4em;
+  margin: 1vh 0;
+}
+
+.botones-popup {
+  display: flex;
+  gap: 1vh;
+  justify-content: center;
+  margin-top: 3vh;
+  font-family: 'EmigreTitulo', sans-serif !important
+}
+
+.botones-popup button {
+  width: 45%;
+  font-family: 'EmigreTitulo', sans-serif !important
+}
+
+.boton-iniciar,
+.boton-salir {
+  flex: 1;  
+  border: none;
+  padding: 1.4vh 2.8vh;
+  border-radius: 1vh;
+  font-size: 2vh;
+  cursor: pointer;
+  transition: background 0.2s ease;
+}
+
+.boton-iniciar {
+  background-color: #EB6608;
+  color: #fff;
+}
+
+.naranja{
+  color: #EB6608;
+}
+.boton-salir {
+  background-color: #1A1413;
+  color: #fff;
+}
+
+.popup-icono {
+  object-fit: contain;
+  margin: 0 auto 0.5svh;
+  display: block;
+  animation: sacudidaError 0.4s ease-in-out 1, flameLoop 1.6s ease-in-out infinite;
+}
+
+@keyframes caerRebotar {
+  0% {
+    transform: translateY(-40px);
+    opacity: 0;
+  }
+  60% {
+    transform: translateY(10px);
+  }
+  80% {
+    transform: translateY(-5px);
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="home-container">
+    <div class="contenido">
+      <img src="/images/logo.png" alt="Logo Little Caesars" class="logo-img" />
+      <img src="/images/titulohome.png" alt="Título del juego" class="titulo-img" />
+      <img src="/images/60-seg-home.png" alt="60 Segundos" class="seg-home-img" />
+      <div class="insignia-contenedor">
+        <img src="/images/insignia-home.png" alt="Insignia" class="insignia-img" />
+        <div class="botones">
+          <button class="btn btn-blanco" @click="iniciarJuego()">
+              COMENZAR
+          </button>
+        </div>
+      </div>      
+    </div>   
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    iniciarJuego() {
+      this.mostrarPopup = false
+      this.$router.push(`/juego`)     
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home-container {
+  background-image: url('/images/fondo.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.contenido {
+  width: 100%;
+  max-width: 45vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 89vh;
+  box-sizing: border-box;
+  text-align: center;
+  color: white;  
+}
+
+.titulo-img {
+  width: 100%;
+  margin-top: 3rem;
+}
+
+
+.seg-home-img {
+  margin-top: 1.5vh;
+}
+
+.insignia-contenedor {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 123%;
+}
+
+.insignia-img {
+  margin-top: 1rem;
+  width: 120%;
+  display: block;
+}
+
+
+.btn {
+  position: absolute;
+  width: 55%;
+  height: 9rem;
+  bottom: 10%; /* ajusta esto según dónde lo quieras */
+  left: 50%;
+  transform: translateX(-50%);
+  border: none;
+  cursor: pointer;
+  background-color: #EB6608;
+  font-size: 5vh; /* más pequeño */
+  color: #FFF;
+  margin-bottom: 2vh;
+  text-transform: uppercase;
+  font-family: 'EmigreTitulo', sans-serif !important;
+  border-radius: 15px;
+}
+
+
+
+/* Responsivo para celulares pequeños */
+@media (max-width: 480px) {
+  .contenido {
+    height: 95vh;
+    padding: 1.5rem 1rem;
+  }
+
+  .titulo-img {
+    max-width: 70%;
+    margin-bottom: -3rem;
+    margin-top: -3rem;
+  }
+
+   .logo-img {
+    max-width: 85%;
+  }
+
+  .seg-home-img {
+    max-width: 60%;
+  }
+
+   .insignia-contenedor {
+    width: 110%;
+  }
+
+  .insignia-img {
+    width: 110%;
+    display: block;
+  }
+
+  .btn {
+    width: 55%;
+    height: 6rem;
+    bottom: 10%; /* ajusta esto según dónde lo quieras */
+    left: 50%;
+    font-size: 4.5vh; /* más pequeño */
+    margin-bottom: -5vh;
+    border-radius: 15px;
+  }
+
+}
+</style>
